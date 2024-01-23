d5eb78e8a39305a5d06f0db2fac90861128c1348
cbarba414 <cristinabarba846@gmail.com>
2024-01-23 10:34:23 -0800

    sxchool but with the new in class edits to create random color schemenes

--- core/src/com/bmhs/gdxintro/AppHandler.java
+++ core/src/com/bmhs/gdxintro/AppHandler.java
@@ -32,7 +32,7 @@ public class AppHandler extends ApplicationAdapter {
 								{1,0,0,5,5,5,5,5,0,0,1,1,1,0,5,8,8,8,8,0},
 								{1,1,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1}};
 
-
+//yay
 */
 
 int [][] worldIntArray = new int [12][20];
